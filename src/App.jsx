280b0e2f5643880d83b44967a264d9377c95d8ce
shuvadeepmondal <121492365+shuvadeepmondal@users.noreply.github.com>
2024-06-12 10:56:57 +0530

    Feat:Sign In page Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Form from './components/Form'
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-    <h1 className='text-4xl text-red-400 '>Hello</h1>
-    </>
+    <div className='flex w-full h-screen'>
+      <div className='w-full flex items-center justify-center '> 
+         <Form />
+      </div>
+      <div>
+
+      </div>
+    
+   
+    </div>
   )
 }
 
